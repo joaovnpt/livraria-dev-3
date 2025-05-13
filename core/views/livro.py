f2eca8238351518ea46ad0fd54e8ba9af8013cce
joaovnpt <joaovitoguiotti8@gmail.com>
2025-05-13 16:40:48 -0300

    FEAT: Separate Livro serializers in List and Retrieve

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,12 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroSerializer, LivroListRetrieveSerializer
+from core.serializers import LivroSerializer, LivroRetrieveSerializer, LivroListSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
 
     def get_serializer_class(self):
-        if self.action in {'list', 'retrieve'}:
-            return LivroListRetrieveSerializer
+        if self.action == 'list':
+            return LivroListSerializer
+        elif self.action == 'retrieve':
+            return LivroRetrieveSerializer
         return LivroSerializer
